786dca4104b35a6c316925c74cd8c5ad0210a5c0
Eraknelo <rene.sackers@live.com>
2014-01-20 01:58:47 +0100

    Optimized and adjusted
    Optimized render event
    Optimized and improved resolution change size recalculation
    Decreased the speed you need to be going at to be able to refuel
    Increased refuel rate

--- client/finite_fuel.lua
+++ client/finite_fuel.lua
@@ -15,9 +15,9 @@ function FiniteFuel:__init()
 	-- Timer for refuel
 	self.refuelTimer = Timer()
 	self.refuelTimeout = 500
-	self.refuelRate = 2
+	self.refuelRate = 4
 
-	-- Actions that should be blocked when there is no fuel
+	-- Fuel related vehicle actions
 	self.vehicleActions = {
 		[Action.Accelerate] = true,
 		[Action.Reverse] = true,
@@ -36,23 +36,78 @@ function FiniteFuel:__init()
 	
 	-- Fuel indicator position
 	self.fuelMeterPosition = "BottomCenter" -- Options: BottomLeft, BottomRight, BottomCenter, TopRight, TopCenter
-	self.fuelMeterWidth = 0.2 -- value * screen width
-	self.fuelMeterHeight = 0.03 -- value * screen height
+	self.fuelMeterRelativeWidth = 0.2 -- value * screen width
+	self.fuelMeterRelativeHeight = 0.03 -- value * screen height
 	self.fuelMeterBackground = Color(0, 0, 0, 100)
 	self.fuelMeterForeground = Color(0, 255, 0)
+	self.fuelMeterRelativeTextSize = 0.02 -- value * screen height
 	self.fuelMeterTextColor = Color(255, 255, 255)
+	
+	self.fuelMeterWidth = 0
+	self.fuelMeterHeight = 0
+	self.fuelMeterLeft = 0
+	self.fuelMeterTop = 0
+	self.fuelMeterTextLeft = 0
+	self.fuelMeterTextTop = 0
+	self.fuelMeterIndicatorWidth = 0
+	self.fuelMeterTextSize = 0
+	self.fuelMeterText = "Fuel"
+	
+	self:CalculateMeterPosition({size = Vector2(Render.Width, Render.Height)})
 
 	-- Events
 	Network:Subscribe("FiniteFuelVehicleFuel", self, self.VehicleFuel)
 	Events:Subscribe("LocalPlayerEnterVehicle", self, self.LocalPlayerEnterVehicle)
 	Events:Subscribe("LocalPlayerExitVehicle", self, self.LocalPlayerExitVehicle)
 	Events:Subscribe("LocalPlayerInput", self, self.LocalPlayerInput)
+	Events:Subscribe("PreTick", self, self.PreTick)
 	Events:Subscribe("Render", self, self.Render)
+	Events:Subscribe("ResolutionChange", self, self.CalculateMeterPosition)
 	
 	-- If player currently in vehicle, call the LocalPlayerEnterVehicle, to get the current vehicle's fuel
 	if LocalPlayer:InVehicle() and LocalPlayer:GetState() == PlayerState.InVehicle and IsValid(LocalPlayer:GetVehicle()) then
 		self:LocalPlayerEnterVehicle({vehicle = LocalPlayer:GetVehicle(), is_driver = true})
 	end
+end
+
+function FiniteFuel:CalculateMeterPosition(args)
+	local size = args.size
+
+	-- Calculate width and height
+	self.fuelMeterWidth = size.x * self.fuelMeterRelativeWidth
+	self.fuelMeterHeight = size.y * self.fuelMeterRelativeHeight
+	
+	-- Calculate left and top positions
+	self.fuelMeterLeft = 0
+	self.fuelMeterTop = 0
+	
+	if self.fuelMeterPosition == "BottomLeft" then
+		self.fuelMeterLeft = 0
+		self.fuelMeterTop = size.y - self.fuelMeterHeight
+	elseif self.fuelMeterPosition == "BottomRight" then
+		self.fuelMeterLeft = size.x - self.fuelMeterWidth
+		self.fuelMeterTop = size.y - self.fuelMeterHeight
+	elseif self.fuelMeterPosition == "BottomCenter" then
+		self.fuelMeterLeft = (size.x / 2) - (self.fuelMeterWidth / 2)
+		self.fuelMeterTop = size.y - self.fuelMeterHeight
+	elseif self.fuelMeterPosition == "TopRight" then
+		self.fuelMeterLeft = size.x - self.fuelMeterWidth
+		self.fuelMeterTop = 0
+	elseif self.fuelMeterPosition == "TopCenter" then
+		self.fuelMeterLeft = (size.x / 2) - (self.fuelMeterWidth / 2)
+		self.fuelMeterTop = 0
+	end
+	
+	self.fuelMeterTextSize = size.y * self.fuelMeterRelativeTextSize
+	
+	self:CalculateTextPosition()
+end
+
+function FiniteFuel:CalculateTextPosition()
+	local textSize = Render:GetTextSize(self.fuelMeterText, self.fuelMeterTextSize)
+	
+	self.fuelMeterTextLeft = self.fuelMeterLeft + (self.fuelMeterWidth / 2) - (textSize.x / 2)
+	self.fuelMeterTextTop = self.fuelMeterTop + (self.fuelMeterHeight / 2) - (textSize.y / 2)
 end
 
 function FiniteFuel:LocalPlayerEnterVehicle(args)
@@ -127,44 +182,28 @@ function FiniteFuel:VehicleFuel(args)
 	Chat:Print("Vehicle currently has " .. fuel .. "/" .. tankSize .. " fuel.", Color(255, 0, 0))
 end
 
+function FiniteFuel:PreTick()
+	-- Check if vehicle ID and tank size are valid and if the GUI state is game, if not, exit function
+	if self.currentVehicle == nil or not IsValid(self.currentVehicle) or self.currentVehicleTankSize == 0 or Game:GetState() ~= GUIState.Game then return end
+	
+	-- Calculate fuel indicator position
+	self.fuelMeterIndicatorWidth = self.fuelMeterWidth / self.currentVehicleTankSize * self.currentVehicleFuel
+end
+
 function FiniteFuel:Render()
 	-- Check if vehicle ID and tank size are valid and if the GUI state is game, if not, exit function
 	if self.currentVehicle == nil or not IsValid(self.currentVehicle) or self.currentVehicleTankSize == 0 or Game:GetState() ~= GUIState.Game then return end
-
-	-- Calculate width and height
-	local meterWidth = Render.Width * self.fuelMeterWidth
-	local meterHeight = Render.Height * self.fuelMeterHeight
-	
-	-- Calculate left and top positions
-	local leftPosition = 0
-	local topPosition = 0
-	
-	if self.fuelMeterPosition == "BottomLeft" then
-		leftPosition = 0
-		topPosition = Render.Height - meterHeight
-	elseif self.fuelMeterPosition == "BottomRight" then
-		leftPosition = Render.Width - meterWidth
-		topPosition = Render.Height - meterHeight
-	elseif self.fuelMeterPosition == "BottomCenter" then
-		leftPosition = (Render.Width / 2) - (meterWidth / 2)
-		topPosition = Render.Height - meterHeight
-	elseif self.fuelMeterPosition == "TopRight" then
-		leftPosition = Render.Width - meterWidth
-		topPosition = 0
-	elseif self.fuelMeterPosition == "TopCenter" then
-		leftPosition = (Render.Width / 2) - (meterWidth / 2)
-		topPosition = 0
-	end
 	
 	-- Draw background
-	Render:FillArea(Vector2(leftPosition, topPosition), Vector2(meterWidth, meterHeight), self.fuelMeterBackground)
+	Render:FillArea(Vector2(self.fuelMeterLeft, self.fuelMeterTop), Vector2(self.fuelMeterWidth, self.fuelMeterHeight), self.fuelMeterBackground)
 	
 	-- Draw indicator
-	local meterIndicatorWidth = meterWidth / self.currentVehicleTankSize * self.currentVehicleFuel
-	Render:FillArea(Vector2(leftPosition, topPosition), Vector2(meterIndicatorWidth, meterHeight), self.fuelMeterForeground)
+	Render:FillArea(Vector2(self.fuelMeterLeft, self.fuelMeterTop), Vector2(self.fuelMeterIndicatorWidth, self.fuelMeterHeight), self.fuelMeterForeground)
 	
 	-- If close to gas station, draw ellipse to indicate refueling station
 	local playerPosition = LocalPlayer:GetPosition()
+	local restartTimer = false
+	local refuelling = false
 	for index, position in ipairs(self.gasStations) do
 		local distance = Vector3.Distance(playerPosition, position)
 		if distance < self.gasStationRadius then
@@ -177,18 +216,39 @@ function FiniteFuel:Render()
 			pos3 = position + (Vector3(0, 2, 1) * (distance / 20))
 			Render:FillTriangle(pos1, pos2, pos3, self.gasStationColor)
 			
-			local velocity = self.currentVehicle:GetAngularVelocity()
-			if distance < self.gasStationRefuelRadius and
-				self.refuelTimer:GetMilliseconds() >= self.refuelTimeout and
-				velocity.x + velocity.y < 1 and
-				self.currentVehicleFuel < self.currentVehicleTankSize then
-				self.refuelTimer:Restart()
+			local velocity = self.currentVehicle:GetLinearVelocity():Length()
+			
+			-- Timeout passed
+			if self.refuelTimer:GetMilliseconds() >= self.refuelTimeout then
+				restartTimer = true
 				
-				self.currentVehicleFuel = self.currentVehicleFuel + self.refuelRate
-				if self.currentVehicleFuel > self.currentVehicleTankSize then self.currentVehicleFuel = self.currentVehicleTankSize end
+				-- Vehicle close enough to the fuel station, tank is not full, and standing still
+				if distance < self.gasStationRefuelRadius and velocity < 0.01 and self.currentVehicleFuel < self.currentVehicleTankSize then
+					refuelling = true
+				
+					-- Fuel up vehicle
+					self.currentVehicleFuel = self.currentVehicleFuel + self.refuelRate
+					if self.currentVehicleFuel > self.currentVehicleTankSize then self.currentVehicleFuel = self.currentVehicleTankSize end
+				end
 			end
 		end
 	end
+	
+	-- Only do if the timer has just ticked
+	if restartTimer then
+		if refuelling and self.fuelMeterText ~= "Refuelling" then
+			self.fuelMeterText = "Refuelling"
+			self:CalculateTextPosition()
+		elseif not refuelling and self.fuelMeterText ~= "Fuel" then
+			self.fuelMeterText = "Fuel"
+			self:CalculateTextPosition()
+		end
+	end
+	
+	if restartTimer then self.refuelTimer:Restart() end
+	
+	-- Draw text
+	Render:DrawText(Vector2(self.fuelMeterTextLeft, self.fuelMeterTextTop), self.fuelMeterText, self.fuelMeterTextColor, self.fuelMeterTextSize)
 end
 
 -- Initialize when module is fully loaded
